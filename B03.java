60a598b11a1eb10618a81176f0d111619405e9cf
cho <aa960411@naver.com>
2016-09-23 23:58:43 +0900

    B03.java 성공

---
+++ B03.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ import java.io.*;
+  class B03{
+           void input(){
+	                    Scanner s = new Scanner(System.in);
+			                     System.out.print("Input Count Day:");
+					                      int day = s.nextInt();
+							                       int seconds = day*24*60*60;
+									                        print(day+"Day is "+seconds+" Seconds");
+														 int mcount = seconds / 1000000;
+														 		 print("It's has 1000000 seconds  " + mcount +"  times");
+																          }
+
+																	          void print(String str){
+																		                   System.out.println(str);
+																				            }
+
+																					            public static void main(String [] args) throws IOException{
+																						                     
+																								                      new B03().input();
+																										               }
+																											        }
+
